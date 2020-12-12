cc09ed104a6f1b88a48c09fdd40ac3ea93c8f74a
Jonathan Sack <jonathan.sack91@gmail.com>
2020-12-11 20:14:44 -0600

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/JSConstraints/JSConstraints-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JSConstraints : NSObject
+@end
+@implementation PodsDummy_JSConstraints
+@end
---
+++ Example/Pods/Target Support Files/Pods-JSConstraints_Example/Pods-JSConstraints_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JSConstraints_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JSConstraints_Example
+@end
